167f345923d6405a5b7210f7c6a48f32518f2f68
Abhinav Mani <abhinav1946@gmail.com>
2020-02-08 20:50:41 +0530

    Data Persistence Added

--- app/src/main/java/com/androlord/farmerapp/Utils/SetPersistence.java
+++ app/src/main/java/com/androlord/farmerapp/Utils/SetPersistence.java
@@ -1,9 +1,13 @@
 package com.androlord.farmerapp.Utils;
 
+import android.app.Application;
+
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SetPersistence {
-    public SetPersistence(){
+public class SetPersistence extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
